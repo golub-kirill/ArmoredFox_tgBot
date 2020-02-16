d5a34d1e1f2c4a7a8f507d1c286be2974959431b
Fox_x <foxxx2game@gmail.com>
2020-02-16 09:12:35 +0200

    поправил кнопы, пилю реквест номера

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,7 +1,13 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+
 
 
 public class Bot extends TelegramLongPollingBot {
@@ -9,20 +15,49 @@ public class Bot extends TelegramLongPollingBot {
     private static final String BOT_USERNAME = "@ArmoredFox_bot";
     private static final String BOT_TOKEN = "1017642766:AAGDA0446HoM8cLGzUTHmmmyQ2krLKXT3tA";
 
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
     public void onUpdateReceived(Update update) {
 
         SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
         String message = update.getMessage().getText();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        int messageID = update.getMessage().getMessageId();
+
+        if (message.equals("/replykeyboard")) {
+            sendMessage.setText("Выбери самую красивую:");
+
+            ArrayList<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+            KeyboardRow keyboardFirstRow = new KeyboardRow();
+            KeyboardRow keyboardSecondRow = new KeyboardRow();
+            keyboard.clear();
+            keyboardFirstRow.add(new KeyboardButton().setText("Share your number >").setRequestContact(true)); //TODO починить это блэд.
+            keyboardSecondRow.add(new KeyboardButton().setText("А я втарая кнапка"));
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+
+            replyKeyboardMarkup.setKeyboard(keyboard);
+        }
 
         if (message.equals("/start")) {
             sendMessage.enableMarkdown(true).setText
                     ("Привет! Я бот @ArmoredFox. " + "\n" + "\n" +
                             "Меня создал [Fox_x](tg://user?id=282614062), но пока он не знает для чего." + "\n" +
-                            "Сейчас я умею совсем немного, но постоянно учусь чему-то новому.");
-        } else if (message.contains("ривет") && !message.contains(" ")) {
-            sendMessage.setText("Приветик \uD83D\uDC4B");
-        } else sendMessage.setText("Я не понимаю, но очень пытаюсь.");
+                            "Сейчас я умею совсем немного, но постоянно учусь чему-то новому.");}
 
+
+
+        if (message.contains("ривет") && !message.contains(" ")) {
+            sendMessage.setText("Приветик \uD83D\uDC4B").setReplyToMessageId(messageID);
+        }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         try {
             execute(sendMessage);
@@ -30,7 +65,7 @@ public class Bot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public String getBotUsername() {
         return BOT_USERNAME;
     }
@@ -38,10 +73,5 @@ public class Bot extends TelegramLongPollingBot {
     public String getBotToken() {
         return BOT_TOKEN;
     }
-        /*if (message.equals("/start")){
-                sendMessage.enableMarkdown(true).setText
-                        ("Привет! Я бот @ArmoredFox. " + "\n" + "\n" +
-                        "Меня создал [Fox_x](tg://user?id=282614062), но пока он не знает для чего." + "\n" +
-                        "Сейчас я умею совсем немного, но постоянно учусь чему-то новому.");
-        }*/
+
 }
