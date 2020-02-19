8ee48ac5960dc60614553b2f7baf6662a52a650b
Fox_x <foxxx2game@gmail.com>
2020-02-19 02:58:32 +0200

    Добавил переменные, /id, обработал исключения.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,11 +2,8 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Bot extends TelegramLongPollingBot {
@@ -18,17 +15,22 @@ public class Bot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
 
-        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
-        String message = update.getMessage().getText();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        int messageID = update.getMessage().getMessageId();
+        long authorID = update.getMessage().getFrom().getId().longValue();
+        long chatID = update.getMessage().getChatId();
+        String authorName = update.getMessage().getFrom().getFirstName();
 
+        SendMessage sendMessage = new SendMessage().setChatId(chatID);
+        String message = update.getMessage().getText();
+
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
         replyKeyboardMarkup.setOneTimeKeyboard(true);
 
-        int messageID = update.getMessage().getMessageId();
-
-        if (message.equals("/replykeyboard")) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            //replykeyboard
+        /*if (message.equals("/replykeyboard")) {
             sendMessage.setText("Выбери самую красивую:");
 
             ArrayList<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
@@ -41,36 +43,36 @@ public class Bot extends TelegramLongPollingBot {
             keyboard.add(keyboardSecondRow);
 
             replyKeyboardMarkup.setKeyboard(keyboard);
-        }
-        //Поздоровались
-        if (message.equals("/start")) {
-            sendMessage.enableMarkdown(true).setText
-                    ("Привет! Я бот @ArmoredFox. " + "\n" + "\n" +
-                            "Меня создал [Fox_x](tg://user?id=282614062), но пока он не знает для чего." + "\n" +
-                            "Сейчас я умею совсем немного, но постоянно учусь чему-то новому.");}
-
-
-
-        if (message.contains("ривет") && !message.contains(" ")) {
-            sendMessage.setText("Приветик \uD83D\uDC4B").setReplyToMessageId(messageID);
-        }
-        //Во сколько на работу.
-        // TODO разлбраться с установкой будильника.
-        if (message.equalsIgnoreCase("На сколько мне сегодня")){
-            Integer dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-            if (dayOfWeek.equals(6)||dayOfWeek.equals(7)){
-                sendMessage.setText("Тебе сегодня на 21:00");
-
+        }*/
+            //Поздоровались
+            if (message.contains("start")) {
+                sendMessage.enableMarkdown(true).setText
+                        ("Привет! Я бот @ArmoredFox. " + "\n" + "\n" +
+                                "Меня создал [Fox_x](tg://user?id=282614062), но пока он не знает для чего." + "\n" +
+                                "Сейчас я умею совсем немного, но постоянно учусь чему-то новому.");
             }
-            else sendMessage.setText("Тебе сегодня на 20:00");
-        }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //Во сколько на работу.
+            else if (message.contains("На сколько мне сегодня") || message.equals("/time")) {
+                Integer dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+                if (dayOfWeek.equals(6) || dayOfWeek.equals(7)) {
+                    sendMessage.enableMarkdown(true).setText("Тебе сегодня на [21:00]" +
+                            "(https://docs.google.com/spreadsheets/d/1Fh72OSGcpNXXPduv734fC0x5CN3T4qzzJCuAkW6yNp4/)");
+                } else sendMessage.enableMarkdown(true).setText("Тебе сегодня на [20:00]" +
+                        "(https://docs.google.com/spreadsheets/d/1Fh72OSGcpNXXPduv734fC0x5CN3T4qzzJCuAkW6yNp4/)");
+            }
+            //Твой id.
+            else if (message.contains("/id")){
+                sendMessage.setText(authorName + " ,твой ID: " + authorID);
+                System.out.println(authorName + "'s chat ID is - " + authorID);
+            }
 
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            else sendMessage.setText("Я не понимаю \uD83D\uDE13");
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        try {execute(sendMessage); }
+
+        catch (TelegramApiException e) { e.printStackTrace(); }
         }
+
     }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public String getBotUsername() {
@@ -80,5 +82,4 @@ public class Bot extends TelegramLongPollingBot {
     public String getBotToken() {
         return BOT_TOKEN;
     }
-
 }
