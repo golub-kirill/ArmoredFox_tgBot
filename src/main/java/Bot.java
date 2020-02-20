a7ccc1ae4611e180b4296a18bcd45a58cceec3d7
Fox_x <foxxx2game@gmail.com>
2020-02-20 02:51:43 +0200

    Допилил получение номера.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,6 +2,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -21,33 +22,35 @@ public class Bot extends TelegramLongPollingBot {
         long authorID = update.getMessage().getFrom().getId().longValue();
         long chatID = update.getMessage().getChatId();
         String authorName = update.getMessage().getFrom().getFirstName();
+        String authorPhoneNumber;
 
         SendMessage sendMessage = new SendMessage().setChatId(chatID);
         String message = update.getMessage().getText();
 
         ArrayList<KeyboardRow> keyboard = new ArrayList<>();
         KeyboardRow keyboardFirstRow = new KeyboardRow();
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
 
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
 
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(true);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //Есть ли сообщение и есть ли в нём текст.
-        if (update.hasMessage() || update.getMessage().hasText()) {
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
 
-            switch (message){
+        //Есть ли сообщение и есть ли в нём текст.
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            switch (message) {
                 //Поздоровались.
-                case "/start":{
+                case "/start": {
                     try {
-                    execute(new SendMessage().setChatId(chatID).enableMarkdown(true).setText(
-                            "Привет! Я бот @ArmoredFox."+ "\n"+"\n"+
-                                    "Меня создал [Fox_x](tg://user?id=282614062), но пока он не знает для чего."+"\n"+
-                                    "Сейчас я умею совсем немного, но постоянно учусь чему-то новому."));
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
+                        execute(new SendMessage().setChatId(chatID).enableMarkdown(true).setText(
+                                "Привет! Я бот @ArmoredFox." + "\n" + "\n" +
+                                        "Меня создал [Fox_x](tg://user?id=282614062), но пока он не знает для чего." + "\n" +
+                                        "Сейчас я умею совсем немного, но постоянно учусь чему-то новому."));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
                     //Задержка в пол секунды перед вторым сообщением.
                     try {
                         Thread.sleep(500);
@@ -57,42 +60,53 @@ public class Bot extends TelegramLongPollingBot {
                     sendMessage.setText("Поделись номером");
 
                     keyboard.clear();
-                    keyboardFirstRow.add(new KeyboardButton().setText("Share your number >").setRequestContact(true));
+                    keyboardFirstRow.add(new KeyboardButton().setText("Поделится номером \uD83D\uDCF1").setRequestContact(true));
+                    keyboardSecondRow.add(new KeyboardButton().setText("Не буду \uD83D\uDD12"));
                     keyboard.add(keyboardFirstRow);
+                    keyboard.add(keyboardSecondRow);
                     replyKeyboardMarkup.setKeyboard(keyboard);
                 } break;
                 //Во сколько на работу.
                 case "На сколько мне сегодня":
                 case "/time": {
                     Integer dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-                        if (dayOfWeek.equals(6) || dayOfWeek.equals(7)) {
-                            sendMessage.enableMarkdown(true).setText("Тебе сегодня на [21:00]" +
-                                    "(https://docs.google.com/spreadsheets/d/1Fh72OSGcpNXXPduv734fC0x5CN3T4qzzJCuAkW6yNp4/)");
-                        } else sendMessage.enableMarkdown(true).setText("Тебе сегодня на [20:00]" +
-                                "(https://docs.google.com/spreadsheets/d/1Fh72OSGcpNXXPduv734fC0x5CN3T4qzzJCuAkW6yNp4/)");} break;
+                    if (dayOfWeek.equals(6) || dayOfWeek.equals(7)) {
+                        sendMessage.enableMarkdown(true).setText("Тебе сегодня на [21:00]" +
+                                "(https://docs.google.com/spreadsheets/d/1Fh72OSGcpNXXPduv734fC0x5CN3T4qzzJCuAkW6yNp4/)");
+                    } else sendMessage.enableMarkdown(true).setText("Тебе сегодня на [20:00]" +
+                            "(https://docs.google.com/spreadsheets/d/1Fh72OSGcpNXXPduv734fC0x5CN3T4qzzJCuAkW6yNp4/)");
+                } break;
                 //Твой ID.
-                case "/id":{
+                case "/id": {
                     sendMessage.setText(authorName + " ,твой ID: " + authorID).setReplyToMessageId(messageID);
                     System.out.println(authorName + "'s chat ID is - " + authorID);
                 } break;
-                default: sendMessage.setText("Я не понимаю \uD83D\uDE13");
+                case "Не буду \uD83D\uDD12": {
+                    sendMessage.setText("Окей \uD83D\uDE14").setReplyMarkup(new ReplyKeyboardRemove().setSelective(true));
+                } break;
+                default:
+                    sendMessage.setText("Я не понимаю \uD83D\uDE13");
             }
         }
         //Если в сообщении нет текста
-        else sendMessage.setText("Я не понимаю \uD83D\uDE13");
-
-        // Получаем номер телефона.
-        if (update.getMessage().hasContact()){
-            String phoneNumber = update.getMessage().getContact().getPhoneNumber();
-            System.out.println(phoneNumber);
-            sendMessage.setText(authorName+", твой номер телефона:" + phoneNumber);
+        else {
+            sendMessage.setText("Я не понимаю \uD83D\uDE13");
         }
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        try {execute(sendMessage); }
-
-        catch (TelegramApiException e) { e.printStackTrace(); }
-    }
+            // Получаем номер телефона.
+            if (update.getMessage().hasContact()) {
+                authorPhoneNumber = update.getMessage().getContact().getPhoneNumber();
+                System.out.println(authorPhoneNumber);
+                //отправляем ответ и удаляем кнопки.
+                sendMessage.setText(authorName + ", твой номер телефона:" + authorPhoneNumber).setReplyMarkup(new ReplyKeyboardRemove().setSelective(true));
+            }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
     public String getBotUsername() {
         return BOT_USERNAME;
     }
