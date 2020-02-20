6fd1fd0b548b7b3c970d3749968409f484d12ac7
Fox_x <foxxx2game@gmail.com>
2020-02-20 12:46:48 +0200

    Подключились к БД.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,20 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
 
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         ApiContextInitializer.init();
+
+        DataBase dataBase = new DataBase();
+        try {
+            dataBase.DbConnector();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         try {
             telegramBotsApi.registerBot(new Bot());
